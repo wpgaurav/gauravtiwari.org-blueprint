f3f8e00157e630270c456fea537ebf99cc3edb1c
Gaurav Tiwari <gaurav@gauravtiwari.org>
2020-09-05 18:55:59 +0530

    Create writ-cta.php

---
+++ writ-cta.php
@@ -0,0 +1,17 @@
+<div class="cta-box cta-full block-single" style="background-color:#ffffff;color:#111111;">
+	<div class="inner">
+		<div class="cta-inner format">
+							<h3 class="cta-title large-title">Writ: Free SEO &amp; Schema WordPress Theme</h3>
+										<div class="cta-image alignleft" style="width:45%;">
+					<img width="798" height="627" src="https://gauravtiwari.org/wp-content/uploads/2020/09/writ.png" class="jetpack-lazy-image jetpack-lazy-image--handled" alt="" loading="lazy" srcset="https://i0.wp.com/gauravtiwari.org/wp-content/uploads/2020/09/writ.png?w=798&amp;ssl=1 798w, https://i0.wp.com/gauravtiwari.org/wp-content/uploads/2020/09/writ.png?resize=764%2C600&amp;ssl=1 764w" data-lazy-loaded="1" sizes="(max-width: 798px) 100vw, 798px">				</div>
+																		<div class="alignright"><ul class="list-check"><li>For Bloggers, Marketers and Writers
+</li><li>Schema integrated
+</li><li>SEO ready
+</li><li>Yoast, RankMath, Jetpack, Elementor compatible
+</li><li>Five Widgetized areas
+</li><li>White Label
+</li><li>Free support
+</li></ul>
+<a class="button button-arrow" href="/download-writ/" rel="nofollow">Download Theme</a> <a class="button button-arrow" style="background:#fff; color:#333" href="/writ/">More Details</a></div>									</div>
+	</div>
+</div>
